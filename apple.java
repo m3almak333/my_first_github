542e7bc5c9c7ab0cb8f2ae4707c4aa89df1cd108
“Ibrahim <ibrahimghadie03@gmail.com>
2023-09-18 16:59:18 -0400

    first try

---
+++ apple.java
@@ -0,0 +1,12 @@
+/**
+ * apple
+ */
+public class apple {
+
+    public static void main(String[] args) {
+        tuna roro= new tuna();
+        roro.myTuna();
+    
+    }
+    
+}
---
+++ tuna.java
@@ -0,0 +1,8 @@
+public class tuna  {
+
+public int x=2;
+    protected void myTuna(){
+System.out.println(x);
+System.out.println("bismillah");
+    }
+}
